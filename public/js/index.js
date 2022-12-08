449f770eec653746e2ed187b26b6328e0a889d01
Healer <202130212@daelim.ac.kr>
2022-12-08 12:44:41 +0900

    for repair

--- public/js/index.js
+++ public/js/index.js
@@ -26,10 +26,10 @@ userNameElement.onclick = () => {
 const localUserHakbun = localStorage.getItem('userHakbun');         // 학번을 저장할 변수 선언
 const localUserEmail = localStorage.getItem('userEmail');           // 이메일을 저장할 변수 선언
 const userHakbunElement = document.querySelector('#Hakbun');        // 엘리먼트 학번 생성
-const userEmailElement = document.querySelector('#Email');           // 엘리먼트 이메일 생성
-const ChangeUserInfo = document.querySelector('#change');
+const userEmailElement = document.querySelector('#Email');          // 엘리먼트 이메일 생성
+const ChangeUserInfo = document.querySelector('#change');           // 한번에 변경할 change 생성
 
-if (localUserHakbun) {
+if (localUserHakbun) {  
     userHakbunElement.textContent = localUserHakbun;
 }
 
@@ -40,19 +40,24 @@ if (localUserEmail) {
 ChangeUserInfo.onclick = () => {
     const userHakbun = prompt("학번을 입력해주세요.");
     if (isNaN(userHakbun)==true){
+        // userHakbun이 숫자가 아니면 아래를 출력하고, 이벤트 종료
         alert('학번은 숫자만 입력이 가능합니다.');
         exit;
     } else {
+        // userHakbun이 숫자가 맞으면 변수를 저장하고 다음 이벤트 실행
         localStorage.setItem('userHakbun', userHakbun);
     }
 
 
-    const userEmail = prompt("@email.com 을 포함한 이메일을 입력해주세요.");
+    const userEmail = prompt("@ 를 포함한 이메일을 입력해주세요.");
     if(userEmail.indexOf('@') != -1){
+        // userEmail에 '@' 가 있으면 아래 함수를 실행한다.
         localStorage.setItem('userEmail', userEmail);
     } else {
+        // userEmail에 '@' 가 없으면 이메일이 아니라고 출력하고
         alert('이메일이 아닙니다.');
         exit;
+        // 이벤트를 종료한다.
     }
 
     userHakbunElement.textContent = userHakbun;
